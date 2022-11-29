68d820d425cea39fb32a8290580a5cce47143573
enesbugrac <enesbc46@gmail.com>
2022-11-29 12:17:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import styles from "./App.module.css";
+const dummyTextandEmojis = [
+  { text: "Life", _id: "1", like: 5, dislike: 10 },
+  { text: "Test", _id: "2", like: 5, dislike: 10 },
+  { text: "Test2", _id: "3", like: 5, dislike: 10 },
+  { text: "Test3", _id: "4", like: 5, dislike: 10 },
+  { text: "Test4", _id: "5", like: 5, dislike: 10 },
+];
 function App() {
+  const handleClick = (_id: string, operation: string) => {
+    dummyTextandEmojis.map((item) => {
+      if (item._id === _id) {
+        if (operation === "like") {
+          item.like = item.like + 1;
+        } else {
+          item.dislike = item.dislike - 1;
+        }
+      } else {
+        return null;
+      }
+    });
+    console.log(dummyTextandEmojis);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles["App"]}>
+      <h1>SPICA REALTIME UI</h1>
+      <ul className={styles["itemContainer"]}>
+        {dummyTextandEmojis.map((item) => (
+          <li key={item._id}>
+            <p>{item.text}</p>
+            <div className={styles["emojiContainer"]}>
+              <button
+                onClick={() => handleClick(item._id, "like")}
+                className={styles["emojiCount"]}
+              >
+                <span>👍</span>
+                <span>{item.like}</span>
+              </button>
+              <button
+                onClick={() => handleClick(item._id, "dislike")}
+                className={styles["emojiCount"]}
+              >
+                <span>👎</span>
+                <span>{item.dislike}</span>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
